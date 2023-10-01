bb2ed081cccc8487c8881d4d3997fbeb68244853
stefano <stefanoimperiale92@gmail.com>
2023-10-02 00:48:17 +1100

    Minor fix

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 
-EXPOSE 8080
+EXPOSE 8443
 
 CMD ["python", "main.py"]
--- env_variables.py
+++ env_variables.py
@@ -12,7 +12,7 @@ TOKEN = os.getenv("TOKEN")
 USER_ID = os.getenv("USER_ID")
 # Prod env
 SERVER_URL = os.getenv("SERVER_URL")
-SERVER_PORT = os.getenv("PORT", "8080")
+SERVER_PORT = os.getenv("PORT", "8443")
 
 # Spreadsheet env
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
